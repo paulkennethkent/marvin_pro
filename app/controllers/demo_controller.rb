00b6634bf0221621b4fecaf943ea367e54c0855c
Paul Kent <paulkent@Pauls-MacBook-Pro.local>
2014-01-30 08:01:13 +0000

    first commit

---
+++ app/controllers/demo_controller.rb
@@ -0,0 +1,20 @@
+class DemoController < ApplicationController
+  	
+  #layout false 
+
+  def index
+  	#render('hello')
+  end
+
+  def hello
+  	#render('index')
+  	@array = [1,2,3,4,5]
+  	@id = params['id']
+  	@page = params[:page]
+  end
+
+  def other_hello
+  	redirect_to(:controller => 'demo', :action => 'index')
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MarvinPro::Application.config.secret_key_base = '97b55e0af55b91938c1670d014abef9cc075efd014d2d107b46e13a549169a41193d7e4edcf532aebe0783d02f278f4b26d1a30bd97992a2c179a52ee9d17383'
