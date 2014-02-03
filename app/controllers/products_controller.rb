36ec2eb5da613e3902d2c077d9b70f1a8b61855e
Paul Kent <paulkent@Pauls-MacBook-Pro.local>
2014-02-03 00:28:44 +0000

    Proudct now works with  new, create, edit, show and update.. not delete

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
-  
+
   def index
     @products = Product.all 
   end
@@ -9,11 +9,45 @@ class ProductsController < ApplicationController
   end
 
   def new
+    @product = Product.new({:name => "Default"})
+  end
+
+  def create
+    # Instatiate a new object
+    @product = Product.new(product_params)
+    # save the object
+    if @product.save
+      # if save succeeds, redirect to the index action
+      redirect_to(:action => 'index')
+    else
+      # if save fails, redisplay the form so user can fix prolbmes 
+      render('new')
+    end
   end
 
   def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    # Find an existing object using form parameters
+    @product = Product.find(params[:id])
+     #update the object
+    if @product.update_attributes(product_params)
+     #if save succeeds, redirect to the index action
+      redirect_to(:action => 'show', :id => @product.id)
+    else
+      # if save fails, redisplay the form so user can fix prolbmes 
+      render('edit')
+    end
   end
 
   def delete
   end
+
+  private
+    def product_params
+      params.require(:product).permit(:name, :tier)
+    end
 end
+
