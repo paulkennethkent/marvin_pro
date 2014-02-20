d327dd3a139f927231fa36d4985f7d4882875ff9
paulkennethkent <paul.kent@oup.com>
2014-02-20 16:13:07 +0000

    change some styling of the button and layout for tickets

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,10 +3,10 @@ module ApplicationHelper
    def status_tag(string)
      
       if string == 'o' 
-        content_tag(:span, "Open" ,:class => 'status true')
+        content_tag(:span, "O" ,:class => 'badge badge-success')
         
       else 
-        content_tag(:span, "Closed", :class => 'status false')
+        content_tag(:span, "C", :class => 'badge badge-important')
       end
   end
   
