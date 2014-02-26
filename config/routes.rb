7aac3358264fd5b7286b5fca8a3d9e75468dcd6d
paulkennethkent <paul.kent@oup.com>
2014-02-26 16:52:09 +0000

    customer controller now fully fledged

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ MarvinPro::Application.routes.draw do
   # root 'welcome#index'
 
   # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  #   get 'products/:id' => 'catalog#view' 
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
