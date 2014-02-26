7c2fb038153f9ccfa37f5cf4c4171eb1b1b38a95
paulkennethkent <paul.kent@oup.com>
2014-02-26 13:23:45 +0000

    tickets can now be paused and there is now option helpers for the status

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,9 +2,12 @@ module ApplicationHelper
   
    def status_tag(string)
      
-      if string == 'o' 
+     if string == 'Open' 
         content_tag(:span, "O" ,:class => 'badge badge-success')
-        
+    
+     elsif string == 'Paused'
+       content_tag(:span, "P" ,:class => 'badge badge-warning')
+       
       else 
         content_tag(:span, "C", :class => 'badge badge-important')
       end
