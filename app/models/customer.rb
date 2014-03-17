0bc53ca735622abc0a0014cd4f8625a7fd9265fd
Paul Kent <paulkent@Pauls-MacBook-Pro.local>
2014-03-17 08:17:03 +0000

    validations complete

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,6 +3,10 @@ class Customer < ActiveRecord::Base
   has_one :school
   
   EMAIL_REGEX = /\A[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}\Z/i
+  FORBIDDEN_NAMES = ['dick','shit','feck']
+
+
+
   # validates_presence_of :name
   # validates_length_of :name, :maximum => 255
   # validates_presence_of :email
@@ -18,6 +22,21 @@ class Customer < ActiveRecord::Base
   					:length => { :maximum => 100},
   					:format => EMAIL_REGEX,
   					:confirmation => true
+
+  validate :name_is_allowed
+  validate :no_new_users_on_sunday, :on => :create
+
+  def name_is_allowed
+  	if FORBIDDEN_NAMES.include?(name)
+  		errors.add(:name, "has been restricted from use.")
+  	end
+  end
+
+  def no_new_users_on_sunday
+  	if Time.now.wday == 6
+  		errors[:base] << "No new users on Sunday."
+  	end
+  end
 
   
 end
