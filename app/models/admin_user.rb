474687423773a46bcd055bee74c93df8fbef2e08
Paul Kent <paulkent@Pauls-MacBook-Pro.local>
2014-03-24 23:50:25 +0000

    admin user is partially created

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -3,8 +3,16 @@ class AdminUser < ActiveRecord::Base
   has_secure_password
   
   has_many :tickets
+
+  scope :sorted, lambda { order("last_name ASC, first_name ASC") }
+
+  def name
+  	"#{first_name} #{last_name}"
+  	# Or: first_name + ' ' + lasrtname 
+  	# Or: [first_name, last_name].join(' ')
  
 	# To configue a different table name: 
 	# self.table_name = "admin_users"
+  end 
 
 end
