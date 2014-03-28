f0461ae39b6f845505c96dcc47e2f3708d4e84eb
paulkennethkent <paul.kent@oup.com>
2014-03-28 16:31:16 +0000

    hidden field addition

--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,9 @@ MarvinPro::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   
-  #resources :tickets do
-    #get :autocomplete_product_name, :on => :collection
-  #end
+  resources :tickets do
+    get :autocomplete_product_name, :on => :collection
+  end
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
@@ -51,10 +51,6 @@ MarvinPro::Application.routes.draw do
   #     end
   #   end
   
-   resources :tickets do
-      get :autocomplete_product_name, :on => :collection
-   end
-
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
