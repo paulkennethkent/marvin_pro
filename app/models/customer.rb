b0f93ee5ead3a6a1f8f14cd7afb8610dc40e70b8
paulkennethkent <paul.kent@oup.com>
2014-03-31 10:06:02 +0000

    altering table attributes and adding some

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -5,6 +5,7 @@ class Customer < ActiveRecord::Base
   EMAIL_REGEX = /\A[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}\Z/i
   validates_presence_of :name
   validates_length_of :name, :maximum => 255
+  validates_length_of :phone, :maximum => 30
   validates_presence_of :email
   validates_length_of :email, :maximum => 100
   validates_format_of :email, :with => EMAIL_REGEX
