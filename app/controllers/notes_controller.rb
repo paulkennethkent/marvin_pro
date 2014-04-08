eb6d5fe45a2ea2cde625c02bef33671c01036bcd
Paul Kenneth Kent <paul@projectresponsive.com>
2014-04-08 21:26:49 +0100

    ignore these change potentially

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,9 +1,17 @@
 class NotesController < ApplicationController
-  before_action :confirm_logged_in
+
+before_action :confirm_logged_in
   
 def create
   @ticket = Ticket.find(params[:ticket_id])
+
   @note = @ticket.notes.create(params[:note].permit(:admin_user_id, :comment))
+  if @note.save
+  	flash[:notice] = "Note created successfuly."
   redirect_to(:controller => 'tickets', :action => 'show', :id => @ticket)
+	else
+	flash[:notice] = "Ticket NOT created successfuly."
+	redirect_to(:controller => 'tickets', :action => 'show', :id => @ticket)
   end
 end
+
